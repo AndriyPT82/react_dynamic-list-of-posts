ec7059082af1c90ca1e767948eae06dd449ea026
Andriy Ivanyk <andriy@MacBook-Air.home>
2021-07-15 00:05:28 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "./precache-manifest.a5b3319c2221808de914225952625c23.js"
 );
 
 self.addEventListener('message', (event) => {
